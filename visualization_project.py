8355173980c5b3830b3552feafb8fc273ef37350
bardo2505 <85026689+bardo2505@users.noreply.github.com>
2023-06-16 18:35:51 +0300

    Update visualization_project.py

--- visualization_project.py
+++ visualization_project.py
@@ -297,7 +297,7 @@ elif comparison == 'Average':
 # Graph 3 #
 
 with st.container():
-    col1, col2, col3, col4,col5, col6, col7, col8 ,col9 = st.columns(4)
+    col1, col2, col3, col4,col5, col6, col7, col8 ,col9 = st.columns(9)
     classical = col1.checkbox('Classical', key=9)
     edm = col2.checkbox('EDM', key=10)
     folk = col3.checkbox('Folk', key=11)    
