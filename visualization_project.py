5c99593975336c0adfc06ca03cee43556995fe63
bardo2505 <85026689+bardo2505@users.noreply.github.com>
2023-06-16 20:34:15 +0300

    Update visualization_project.py

--- visualization_project.py
+++ visualization_project.py
@@ -126,7 +126,7 @@ if comparison == 'Comparison':
     Anxiety = px.scatter(df,x="Age", y = 'Anxiety',
                         color="Fav genre",
                         title="Age Vs. Anxiety",
-                        color_discrete_sequence = cmap_graph_12)
+                        color_discrete_sequence = px.colors.qualitative.Safe)
 
     
     Depression = px.scatter(df,x="Age", y = 'Depression',
