2767c7b872016212e616a969c3a634d33af07204
bardo2505 <85026689+bardo2505@users.noreply.github.com>
2023-06-18 22:32:44 +0300

    Update visualization_project.py

--- visualization_project.py
+++ visualization_project.py
@@ -17,11 +17,11 @@ from plotly.subplots import make_subplots
 st.set_page_config(page_title="Streamlit Project",
                    page_icon=":bar_chart:",
                   layout="wide")
-st.markdown("<h1 style='text-align: center; color: red;'>Some title</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: black;'>Visualization Final Project</h1>", unsafe_allow_html=True)
 
-with st.container():
-    col1, col2, col3 = st.columns([0.25, 0.5, 0.25])
-    col2.title('Visualization Final Project')
+# with st.container():
+#     col1, col2, col3 = st.columns([0.25, 0.5, 0.25])
+#     col2.title('Visualization Final Project')
 with st.container():
     col1, col2, col3 = st.columns([0.30, 0.5, 0.20])
     col2.header('Beni Ifland - 208906255')
