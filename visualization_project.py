9a673122d12d93f04a80ccf8ca12f48c5d90d382
bardo2505 <85026689+bardo2505@users.noreply.github.com>
2023-06-19 14:25:05 +0300

    Update visualization_project.py

--- visualization_project.py
+++ visualization_project.py
@@ -21,7 +21,7 @@ st.markdown("<h1 style='text-align: center; color: black;'>Visualization Final P
 st.markdown("<h1 style='text-align: center; color: black;'>Beni Ifland - 208906255</h1>", unsafe_allow_html=True)
 st.markdown("<h1 style='text-align: center; color: black;'>Bar Dolev - 318419512</h1>", unsafe_allow_html=True)
 
-st.markdown("<style>h2{font-size: 16px; font-family: Assistant, sans-serif;}</style>", unsafe_allow_html=True) 
+st.markdown("<style>h2{font-size: 18px; font-family: Assistant, sans-serif;}</style>", unsafe_allow_html=True) 
 st.markdown("<style>h3{font-size: 20px; font-family: Assistant, sans-serif;}</style>", unsafe_allow_html=True) 
 st.markdown("<style>h4{font-size: 26px; font-family: Assistant, sans-serif;}</style>", unsafe_allow_html=True) 
 
@@ -32,7 +32,8 @@ st.markdown("<h3>The main question we chose is whether there is a connection bet
 # st.subheader("""""")
 
 st.markdown("---")  
-color_blind = st.radio("This Project is Color-blind friendly, Are you color blind?",['No','Yes'],key=51) # Did you know that 9% of men are color blind?
+
+color_blind = st.radio("<h2>This Project is Color-blind friendly, Are you color blind?</h2>",['No','Yes'],key=51) # Did you know that 9% of men are color blind?
 if color_blind == 'Yes': 
   cmap_graph_4 = "balance" # graph 4
   color_map_graphs12 = {
@@ -132,7 +133,7 @@ st.markdown("---")
 st.subheader('Age & Mental Health Disorders scores - Scatter plot')
 st.markdown("<h2>Would you like to observe the relationships between age and the Average of the Mental health scores? Or compare between two or more specific Mental Disorders?</h2>", unsafe_allow_html=True)
 
-comparison = st.radio("Choose one of:", ['None', 'Average', 'Comparison'], key=50)
+comparison = st.radio("<h2>Choose one of:</h2>", ['None', 'Average', 'Comparison'], key=50)
   
 if comparison == 'Comparison':
     st.markdown("<h2>Which Mental Health Disorders would you like to compare?</h2>", unsafe_allow_html=True)
@@ -295,8 +296,8 @@ if comparison == 'Comparison':
   
   
 elif comparison == 'Average':
-      st.markdown("<h2>Choose view method for genres:</h2>", unsafe_allow_html=True)
-      bool_genres = st.radio("",['I prefer to choose the genres myself','Select all genres'],key=779)
+
+      bool_genres = st.radio("<h2>Choose view method for genres:</h2>",['I prefer to choose the genres myself','Select all genres'],key=779)
       if bool_genres=='I prefer to choose the genres myself':
         with st.container():
           col1, col2, col3, col4,col5, col6, col7, col8 ,col9 = st.columns(9)
@@ -355,8 +356,7 @@ st.markdown("---")
 st.subheader('Hours of listening (Daily) & Mental Health Disorders scores - Scatter plot')
 st.markdown("<h2>Would you like to observe the relationships between Hours of listening per day and the Average of the Mental health scores? Or compare between two or more specific Mental Disorders?</h2>", unsafe_allow_html=True)
 
-st.markdown("<h2>Choose one of:</h2>", unsafe_allow_html=True)
-comparison = st.radio("", ['None', 'Average', 'Comparison'], key=52)
+comparison = st.radio("<h2>Choose one of:</h2>", ['None', 'Average', 'Comparison'], key=52)
 if comparison == 'Comparison':
     st.markdown("<h2>Which Mental Health Disorders would you like to compare?</h2>", unsafe_allow_html=True)
     with st.container():
@@ -394,8 +394,8 @@ if comparison == 'Comparison':
 
     if sum(list_of_trues) > 0:
       
-      st.markdown("<h2>Please choose view method for genres:</h2>", unsafe_allow_html=True)
-      bool_genres2 = st.radio("",['I prefer to choose the genres myself','Select all genres'],key=877)
+
+      bool_genres2 = st.radio("<h2>Please choose view method for genres:</h2>",['I prefer to choose the genres myself','Select all genres'],key=877)
       if bool_genres2=='I prefer to choose the genres myself':
         with st.container():
           col1, col2, col3, col4,col5, col6, col7, col8 ,col9 = st.columns(9)
@@ -504,8 +504,8 @@ if comparison == 'Comparison':
 
 
 elif comparison == 'Average':
-    st.markdown("<h2>Please choose view method for genres:</h2>", unsafe_allow_html=True)
-    bool_genres2 = st.radio("",['I prefer to choose the genres myself','Select all genres'],key=77229)
+
+    bool_genres2 = st.radio("<h2>Please choose view method for genres:</h2>",['I prefer to choose the genres myself','Select all genres'],key=77229)
     if bool_genres2=='I prefer to choose the genres myself':
         with st.container():
           col1, col2, col3, col4,col5, col6, col7, col8 ,col9 = st.columns(9)
@@ -562,13 +562,13 @@ st.markdown("---")
 ##################################### Third Graph #####################################
 
 st.subheader('Genres & Mental Health Scores, by Mental Health Disorder - Histogram')
-order = st.radio("Which type of view would you prefer?",['Overall view (Compare all 4 disorders)','Specific view (Zoom in on one disorder)'],key=40000)
+order = st.radio("<h2>Which type of view would you prefer?</h2>",['Overall view (Compare all 4 disorders)','Specific view (Zoom in on one disorder)'],key=40000)
 if order != 'Overall view (Compare all 4 disorders)':
-  st.markdown("<h2>Please choose disorder to view:</h2>", unsafe_allow_html=True)
-  disorder = st.radio("",['Anxiety','Depression','Insomnia','OCD'],key=40001)
+
+  disorder = st.radio("<h2>Please choose disorder to view:</h2>",['Anxiety','Depression','Insomnia','OCD'],key=40001)
 st.markdown("<h2>Would you like to view all Genres at once?</h2>", unsafe_allow_html=True)  
-st.markdown("<h2>Choose one of:</h2>", unsafe_allow_html=True)
-select_all = st.radio("",['Yes please.','No, I will choose myself.'])
+
+select_all = st.radio("<h2>Choose one of:</h2>",['Yes please.','No, I will choose myself.'])
 if select_all == 'Yes please.': 
   with st.container():
       col1, col2, col3, col4,col5, col6, col7, col8 ,col9 = st.columns(9)
